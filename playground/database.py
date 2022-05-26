16ae75ecdea2b76b3c6024f5531f7454cfc647e3
requinard <david@requinard.nl>
2022-05-26 22:31:48 +0200

    Add a database connection and some small tests to assert it

---
+++ playground/database.py
@@ -0,0 +1,27 @@
+from pydantic.tools import lru_cache
+from sqlalchemy.engine import Engine
+from sqlmodel import create_engine, SQLModel, Session
+
+
+@lru_cache
+def get_database_engine() -> Engine:
+    """
+    Create an engine and memoize the results. This ensures we only create a single engine.
+    """
+    sqlite_file_name = "../database.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+    SQLModel.metadata.create_all(engine)
+
+    return engine
+
+
+def get_session(engine=get_database_engine()) -> Session:
+    """
+    Yield a session that can be used as a FastAPI dependency
+    """
+    with Session(engine) as session:
+        yield session
---
+++ tests/test_database.py
@@ -0,0 +1,14 @@
+from playground.database import get_database_engine, get_session
+
+
+def test_engine_is_cached():
+    e1 = get_database_engine()
+    e2 = get_database_engine()
+
+    assert e1 is e2
+
+def test_session_is_different():
+    s1 = get_session()
+    s2 = get_session()
+
+    assert s1 is not s2
