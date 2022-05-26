e063d821cc2eab0afa790f02e4819c69b05b9ad9
requinard <david@requinard.nl>
2022-05-26 21:53:18 +0200

    Add a readme and prettify the front page

---
+++ README.md
@@ -0,0 +1,3 @@
+# FastAPI Playground
+
+A project where I do some easy stuff with FastAPI that other frameworks would not be able to do
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from paginator import pagination_router
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/", include_in_schema=False)
+def redirect_to_docs():
+    return RedirectResponse("/docs")
 
 
 app.include_router(pagination_router, prefix="/pagination", tags=["Pagination"])
